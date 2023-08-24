a5a59da0e48f50bf36383709f9a6092d67b44799
Pablo Monge <pablomonge55@gmail.com>
2023-08-24 20:48:51 -0300

    ADD: Se reformo el archivo setting

--- src/proy2/settings.py
+++ src/proy2/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIRS = [
+    BASE_DIR / 'STATIC',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
